784787e2a9ad2e7ed65fc50fe689a0f5d435e19e
Nigel Smith <nigsmith@redhat.com>
2025-12-04 14:43:52 +0000

    updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # must-gather_gather_metrics-PromMCPServer_claudeDemo
+
+This repo demonstrates extracting selected metrics from an Openshift cluster.
+The exported metrics are used to create TSDB blocks and expose that data via a containized prometheus instance.
+Claude is then used to query the data via a Prometheus MCP server. 
+
+`oc adm must-gather` can now collect metrics from a given cluster
+This feature is available from 4.18 + 
+
+```
+oc adm must-gather -- gather_metrics \
+--min-time=$(date --date='2 hours ago' +%s%3N) \
+--match="{__name__=~\'kube_node_.*\'}"
+```
+
+Run the must-gather command within this repo or place a pre collected `must-gather -- gather_metrics` directory into the root of this repo.
+
+
+Running the launcher will result in the collected data being available via a running container
+
+```
+./launcher.sh
+```
+
+launch claude and configure the MCP server.
+Tell Claude that is its querying a Prometheus dump containing historical data:
+```
+I see! The Prometheus server contains historical data from a promdump. Let me query with a proper timestamp. I'll calculate 1 hour ago:
+```
+
+Now you can query the data using natural language:
+
+```
+● Great! Found the nodes with master role from 1 hour ago:
+
+  3 Master Nodes:
+
+  1. sharedocp420-sxt45-master-0
+  2. sharedocp420-sxt45-master-1
+  3. sharedocp420-sxt45-master-2
+```
+
+This script touches the file `firstrun.txt` after its firstrun. 
---
+++ launcher.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Clean up
+mkdir -p tmp/
+
+if ! test -f firstrun.txt;
+  then
+    rm -rf tmp/*
+    #Install claude?
+    read -p "This demo requires a claude code subscription - do you want to install claude?" -n 1 -r
+      echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+          then
+            curl -fsSL https://claude.ai/install.sh | bash
+        fi
+
+    #Add Prometheus MCP server to Claude
+    read -p "This demo uses a Prometheus MCP server - do you want to install it?" -n 1 -r
+      echo                     
+        if [[ $REPLY =~ ^[Yy]$ ]]
+          then
+            claude mcp add prometheus --env PROMETHEUS_URL=http://localhost:9090 -- docker run -i --rm -e PROMETHEUS_URL ghcr.io/pab1it0/prometheus-mcp-server:latest  
+        fi
+
+    #Get desired PROM_VERSION
+      echo -n "Insert desired Prometheus version - Match this to the version of the cluster that created the dump. Openshift 4.18 uses '2.51.1' :"
+      read PROM_VERSION
+      echo "PROM_VERSION=$PROM_VERSION" > firstrun.txt
+
+    #Download the desired PROM_VERSION
+      curl -Ls https://github.com/prometheus/prometheus/releases/download/v$PROM_VERSION/prometheus-$PROM_VERSION.linux-amd64.tar.gz | tar -xvz -C tmp
+       touch firstrun.txt
+fi
+
+source firstrun.txt
+echo $PROM_VERSION
+
+#Create configfiles
+mkdir -p tmp/prometheus-config
+touch tmp/prometheus-config/prometheus.yml
+
+#Create tsdb blocks from openmetrics 
+tmp/prometheus-$PROM_VERSION.linux-amd64/promtool tsdb create-blocks-from openmetrics must-gather.local.*/quay-io-openshift-*/monitoring/metrics/metrics.openmetrics tmp/prometheus-$PROM_VERSION.linux-amd64/data/
+
+#Launch the container
+podman run --rm -p 9090:9090/tcp -v $PWD/tmp/prometheus-$PROM_VERSION.linux-amd64/data:/prometheus:U,Z --privileged quay.io/prometheus/prometheus --storage.tsdb.path=/prometheus --config.file=/dev/null
